dd8df3b8f15720789a2fc154e1b4cede719cd8e0
Nando DP <maodo2000diop@gmail.com>
2023-12-19 15:08:55 +0000

    Sommaire

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page, show_vizualisation_page
 
-page = st.sidebar.selectbox('Prédire, explorer ou visualiser', ('Prédiction', 'Exploration', 'Visualisation'))
+# page = st.sidebar.selectbox('Prédire, explorer ou visualiser', ('Prédiction', 'Exploration', 'Visualisation'))
+page = st.sidebar.radio('Sommaire :', ('Prédiction', 'Exploration', 'Visualisation'))
+
 
 if page == 'Prédiction':
     show_predict_page()
--- predict_page.py
+++ predict_page.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def load_model():
     with open('model.sav', 'rb') as file:
-        data = pickle.load(file)
-    return data
+        model = pickle.load(file)
+    return model
 
-data = load_model()
+model = load_model()
 
 def show_predict_page():
     st.title("Prédiction de la quantité de C02 émis des vehicules")
@@ -21,7 +21,7 @@ def show_predict_page():
     click = st.button("Prédire l'émission de C02")
     if click:
         input_data = np.asarray([taille_moteur, nbr_cylindre, cons_carburant]).reshape(1, -1)
-        prediction = data.predict(input_data)
+        prediction = model.predict(input_data)
 
         st.subheader(f"La quantité de C02 émis par ce vehicule est estimer à {prediction[0]:.2f}")
     
